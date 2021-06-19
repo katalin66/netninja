af81696e3f7ba3beeac51793e0a80dc4b0127a66
Katalin Szokoli <szokoli.kati@gmail.com>
2021-06-19 18:57:24 +0200

    .env install

--- node-express-jwt-auth/node-express-jwt-auth-lesson-1/app.js
+++ node-express-jwt-auth/node-express-jwt-auth-lesson-1/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
-
+require('dotenv').config();
 const app = express();
 
 // middleware
@@ -13,7 +13,8 @@ app.set('view engine', 'ejs');
 
 // database connection
 // const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-const dbURI = 'mongodb+srv://mySelf:mySelf2513@cluster1.s2xby.mongodb.net/node-auth';
+// const dbURI = 'mongodb+srv://mySelf:mySelf2513@cluster1.s2xby.mongodb.net/node-auth';
+const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster1.s2xby.mongodb.net/node-auth`;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
