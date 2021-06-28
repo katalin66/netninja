fe2df0b5db169c3b3df8ffeb1ce1263c898f52a6
Katalin Szokoli <szokoli.kati@gmail.com>
2021-06-28 12:28:35 +0200

    modifying until 7.lesson

--- node-express-jwt-auth/node-express-jwt-auth-lesson-1/app.js
+++ node-express-jwt-auth/node-express-jwt-auth-lesson-1/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+
 require('dotenv').config();
 const app = express();
 
 // middleware
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser())
 
 // view engine
 app.set('view engine', 'ejs');
@@ -23,3 +26,20 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes)
+
+//cookies
+app.get('/set-cookies', (req, res) => {
+ 
+//  res.setHeader('Set-Cookie', 'newUser=true');
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', 'true', { maxAge: 1000 * 60 * 60 * 24, httpOnly: true }); //the cookie will removed after 1 day
+
+  res.send('you got the cookies!')
+
+})
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+  res.json(cookies)
+})
