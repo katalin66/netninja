873fa030c950592ee90d3351f6aca5e4e65f013b
Katalin Szokoli <szokoli.kati@gmail.com>
2021-06-29 18:56:58 +0200

    set and read cookies

--- node-express-jwt-auth/node-express-jwt-auth-lesson-1/app.js
+++ node-express-jwt-auth/node-express-jwt-auth-lesson-1/app.js
@@ -28,18 +28,19 @@ app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes)
 
 //cookies
-app.get('/set-cookies', (req, res) => {
+// app.get('/set-cookies', (req, res) => {
  
-//  res.setHeader('Set-Cookie', 'newUser=true');
-  res.cookie('newUser', false);
-  res.cookie('isEmployee', 'true', { maxAge: 1000 * 60 * 60 * 24, httpOnly: true }); //the cookie will removed after 1 day
+// //  res.setHeader('Set-Cookie', 'newUser=true');
+//   res.cookie('newUser', false);
+//   res.cookie('isEmployee', 'true', { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+//    //the cookie will removed after 1 day and not reached from frontend
 
-  res.send('you got the cookies!')
+//   res.send('you got the cookies!')
 
-})
+// })
 
-app.get('/read-cookies', (req, res) => {
-  const cookies = req.cookies;
-  console.log(cookies);
-  res.json(cookies)
-})
+// app.get('/read-cookies', (req, res) => {
+//   const cookies = req.cookies;
+//   console.log(cookies);
+//   res.json(cookies)
+// })
--- node-express-jwt-auth/node-express-jwt-auth-lesson-1/controllers/authController.js
+++ node-express-jwt-auth/node-express-jwt-auth-lesson-1/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const jwt = require('jsonwebtoken');
 
 // handle errors
 const handleErrors = (err) => {
@@ -21,6 +22,11 @@ const handleErrors = (err) => {
   return errors
 }
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, 'net ninja secret', { expiresIn: maxAge})
+}
+
 module.exports.signup_get = (req, res) => {
   res.render('signup')
 }
@@ -33,8 +39,10 @@ module.exports.signup_post = async (req, res) => {
   const {email, password} = req.body;
   // res.send('new signup')
   try {
-    const user = await User.create({email, password});
-    res.status(201).json(user)
+    const user = await User.create({ email, password });
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id })
   }
   catch (err) {
     // console.log(err);
