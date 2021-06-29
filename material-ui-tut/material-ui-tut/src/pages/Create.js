b2b499fd1a38a32a597283202290a9dc246b38ae
Katalin Szokoli <szokoli.kati@gmail.com>
2021-06-29 18:54:03 +0200

    first lesson

---
+++ material-ui-tut/material-ui-tut/src/pages/Create.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import Typography from '@material-ui/core/Typography';
+// import { Typography } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+
+export default function Create() {
+  return (
+    <div>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+    </div>
+  )
+}
